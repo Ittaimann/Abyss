9a0c747cf9153d35f9fe0669e7e991ff96235f6d
Ittaimann <ittaiam@gmail.com>
2017-12-17 17:12:06 -0800

    teste script

---
+++ Abyss/Assets/Scripts/testing.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print(SystemInfo.batteryLevel)s;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+}
