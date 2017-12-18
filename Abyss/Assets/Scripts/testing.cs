8aaa8f3d776bc63e2d93f37f50669dcb77fac7f3
Ittaimann <ittaiam@gmail.com>
2017-12-17 20:03:37 -0800

    simple rotation of object relative to phone

--- Abyss/Assets/Scripts/testing.cs
+++ Abyss/Assets/Scripts/testing.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class testing : MonoBehaviour {
+	private Rigidbody2D rb2D;
+	public float offset;
+    public float speed;
 
-	// Use this for initialization
-	void Start () {
-		print(SystemInfo.batteryLevel)s;
-	}
+
+    // Use this for initialization
+    void Start () {
+        rb2D = GetComponent<Rigidbody2D>();
+		offset=Input.gyro.attitude.eulerAngles.z;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-		
-	}
+		print(Input.gyro.attitude.eulerAngles.z);
+        transform.localEulerAngles=new Vector3(0,0,Input.gyro.attitude.eulerAngles.z-offset);
+
+
+    }
+
+	
 }
