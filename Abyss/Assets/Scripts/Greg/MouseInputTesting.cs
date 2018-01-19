7fb993498a532a1e6d58eace68e27bfcf49b64ce
gregduarte97@gmail.com <Greg D>
2018-01-18 18:41:25 -0800

    Greg -- added a test scene and various assets to start.
      - Built a 9-slice border with colliders so no matter what scale, the border isn't stretched out. (Need to write a script to create and adapt colliders to the border scale, since it should be dynamic in my opinion)
    
      - Added poopy trackpad art assets.

---
+++ Abyss/Assets/Scripts/Greg/MouseInputTesting.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInputTesting : MonoBehaviour {
+
+    public float playerSpeed;
+    public float acceleration;
+
+    Vector2 desiredVelocity, startingVelocity;
+    float movementLerpStep;
+    Vector3 previousMousePos;
+
+    void Start () {
+        desiredVelocity = Vector2.zero;
+        startingVelocity = Vector2.zero;
+    }
+    
+    void Update () {
+
+        // Testing the "each half of the screen moves the character" thing //
+
+        // (in my opinion it should be each half of the player, the screen dictating the
+        // left/right split feels less intuitive... unless perhaps we visually mark the split)
+
+        if (Input.GetMouseButton(0)) {
+            Vector3 currentMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (Mathf.Sign(currentMousePos.x) != Mathf.Sign(previousMousePos.x)) {
+                movementLerpStep = 1 - (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) / playerSpeed);
+                startingVelocity = desiredVelocity;
+                desiredVelocity = new Vector2(playerSpeed * Mathf.Sign(Camera.main.ScreenToWorldPoint(Input.mousePosition).x), desiredVelocity.y);
+            }
+
+            previousMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            movementLerpStep = Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) / playerSpeed;
+            startingVelocity = Vector2.zero;
+            desiredVelocity = new Vector2(playerSpeed * Mathf.Sign(Camera.main.ScreenToWorldPoint(Input.mousePosition).x), desiredVelocity.y);
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            movementLerpStep = 1 - (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) / playerSpeed);
+            startingVelocity = desiredVelocity;
+            desiredVelocity = Vector2.zero;
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(
+            Mathf.SmoothStep(startingVelocity.x, desiredVelocity.x, movementLerpStep),
+            GetComponent<Rigidbody2D>().velocity.y);
+
+        if (movementLerpStep < 1f) {
+            movementLerpStep += Time.deltaTime*acceleration;
+        } else {
+            movementLerpStep = 1f;
+        }
+    }
+}
