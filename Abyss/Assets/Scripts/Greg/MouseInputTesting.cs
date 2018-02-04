cb7a7772727bf4a0265603f7fc4c46daaf31549e
gregduarte97@gmail.com <Greg D>
2018-02-03 20:20:01 -0800

    Super revamped MouseInputTesting

--- Abyss/Assets/Scripts/Greg/MouseInputTesting.cs
+++ Abyss/Assets/Scripts/Greg/MouseInputTesting.cs
@@ -6,13 +6,17 @@ public class MouseInputTesting : MonoBehaviour {
 
     public float playerSpeed;
     public float acceleration;
+    public float doubleTapTime;
+    public float movementIgnoreRadius;
 
-    Vector2 desiredVelocity, startingVelocity;
+    bool isDoubleTap;
+    Coroutine doubleTapCoroutine;
+    Vector2 desiredVelocity, previousDesiredVelocity, startingVelocity;
+    Vector2 prevDirection, direction; // values are: -1 = left on screen, 0 = idle, 1 = right on screen
     float movementLerpStep;
-    Vector3 previousMousePos;
 
     // TODO change this later, of course
-    PrototypeRotatorSystem rotatorSystem;
+    public PrototypeRotatorSystem rotatorSystem;
 
     void Start () {
         desiredVelocity = Vector2.zero;
@@ -20,39 +24,139 @@ public class MouseInputTesting : MonoBehaviour {
     }
     
     void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            if (isDoubleTap) {
+                // If the double-tap coroutine is still running, stop it -- it can cause an issue at a specific edge case
+                StopCoroutine(doubleTapCoroutine);
+                isDoubleTap = false;
+
+                // Jump Logic -- put it here as an AddForce, do some interpolation in Input.GetMouseButton(0), start a coroutine -- anything works
+                Debug.Log("JUMP");
+            } else {
+                doubleTapCoroutine = StartCoroutine(DoubleTapCheck());
+                isDoubleTap = true;
+            }
+        }
+
+        // There's a guard applied to ignore movement when the input is really close to the player's current position (prevents constantly moving)
+        if (!CheckMovementIgnore()) {
+            // Update direction values based on things like rotation direction, input position, and updating based on GetMouseButtonDown vs GetMouseButton
+            RunDirectionHandler();
+
+            // Apply velocity based on movement direction
+            if (rotatorSystem.orientation == 0 || rotatorSystem.orientation == 2) {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(
+                    Mathf.SmoothStep(playerSpeed * prevDirection.x, playerSpeed * direction.x, movementLerpStep),
+                    GetComponent<Rigidbody2D>().velocity.y);
+            } else if (rotatorSystem.orientation == 1 || rotatorSystem.orientation == 3) {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(
+                    GetComponent<Rigidbody2D>().velocity.x,
+                    Mathf.SmoothStep(playerSpeed * prevDirection.y, playerSpeed * direction.y, movementLerpStep));
+            }
+
+            if (movementLerpStep < 1f) {
+                movementLerpStep += Time.deltaTime*acceleration;
+            } else {
+                movementLerpStep = 1f;
+            }
+        }
+        
+    }
+
+    private void RunDirectionHandler() {
+        bool updateStep = false;
+
+        // GetMouseButtonDown is different from GetMouseButton because this is called without any prior movement to account for
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 inputPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            direction = new Vector2(
+                inputPos.x > transform.position.x ?  1 :
+                inputPos.x < transform.position.x ? -1 : 0,
+                inputPos.y > transform.position.y ?  1 :
+                inputPos.y < transform.position.y ? -1 : 0);
+
+            prevDirection = Vector2.zero;
+            updateStep = true;
+        }
+
+        // GetMouseButton is different since we're switching direction, instead of starting to move without a prior direction to remember
+        if (Input.GetMouseButton(0)) {
+            Vector3 inputPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // This looks complicated -- it's just ~50 lines of nested if-statements condensed down into some ternary operations
+
+            // We could be getting GetMouseButton without having done GetMouseButtonDown first -- it's copy/paste but it's minor
+            if (direction.x == 0 || direction.y == 0) {
+                direction = new Vector2(
+                    inputPos.x > transform.position.x ?  1 :
+                    inputPos.x < transform.position.x ? -1 : 0,
+                    inputPos.y > transform.position.y ?  1 :
+                    inputPos.y < transform.position.y ? -1 : 0);
+                prevDirection = Vector2.zero;
+                updateStep = true;
+            } else {
+                Vector2 lastDirection = direction;
+                direction = new Vector2(
+                    inputPos.x > transform.position.x && direction.x == -1 ? 1 :
+                    inputPos.x < transform.position.x && direction.x ==  1 ? -1 :
+                    direction.x,
+                    inputPos.y > transform.position.y && direction.y == -1 ? 1 :
+                    inputPos.y < transform.position.y && direction.y ==  1 ? -1 :
+                    direction.y);
+                if ((rotatorSystem.orientation == 0 || rotatorSystem.orientation == 2) && lastDirection.x != direction.x ||
+                    (rotatorSystem.orientation == 1 || rotatorSystem.orientation == 3) && lastDirection.y != direction.y)
+                    updateStep = true;
+            }
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            prevDirection = direction;
+            direction = Vector2.zero;
+            updateStep = true;
+        }
+
+        // Updates the Lerp interval when a change in direction has been initiated.
+
+        // For example: say our movement lerp looked like Lerp(0, speed, 0.8) where speed = 5 (Lerp would return 4)
+        // and then we wanted to go the other way smoothly. Our new lerp would have to be (speed, -speed, X) where X would return
+        // the same value for Lerp as it did previously (4)... so we calculate that here.
+        if (updateStep) {
+            updateStep = false;
+            if (rotatorSystem.orientation == 0 || rotatorSystem.orientation == 2) {
+                // set movementLerpStep to 0 when a divide by zero error would occur
+                if (startingVelocity.x - desiredVelocity.x == 0)
+                    movementLerpStep = 0;
+                else
+                    movementLerpStep = Mathf.Abs((startingVelocity.x - GetComponent<Rigidbody2D>().velocity.x) / (startingVelocity.x - desiredVelocity.x));
+            } else if (rotatorSystem.orientation == 1 || rotatorSystem.orientation == 3) {
+                if (startingVelocity.y - desiredVelocity.y == 0)
+                    movementLerpStep = 0;
+                else
+                    movementLerpStep = Mathf.Abs((startingVelocity.y - GetComponent<Rigidbody2D>().velocity.y) / (startingVelocity.y - desiredVelocity.y));
+            }
+        }
+    }
+
+    private IEnumerator DoubleTapCheck() {
+        yield return new WaitForSeconds(doubleTapTime);
+        isDoubleTap = false;
+    }
+
+    private bool CheckMovementIgnore() {
+        bool ignore = false;
 
         if (Input.GetMouseButton(0)) {
-            Vector3 currentMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 inputPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            if (Mathf.Sign(currentMousePos.x) != Mathf.Sign(previousMousePos.x)) {
-                movementLerpStep = 1 - (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) / playerSpeed);
-                startingVelocity = desiredVelocity;
-                desiredVelocity = new Vector2(playerSpeed * Mathf.Sign(Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x), desiredVelocity.y);
+            if (rotatorSystem.orientation == 0 || rotatorSystem.orientation == 2) {
+                if (inputPos.x > transform.position.x - movementIgnoreRadius && inputPos.x < transform.position.x + movementIgnoreRadius)
+                    ignore = true;
+            } else if (rotatorSystem.orientation == 1 || rotatorSystem.orientation == 3) {
+                if (inputPos.y > transform.position.y - movementIgnoreRadius && inputPos.y < transform.position.y + movementIgnoreRadius)
+                    ignore = true;
             }
-            previousMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
 
-        if (Input.GetMouseButtonDown(0)) {
-            movementLerpStep = Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) / playerSpeed;
-            startingVelocity = Vector2.zero;
-            desiredVelocity = new Vector2(playerSpeed * Mathf.Sign(Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x), desiredVelocity.y);
-        }
-
-        if (Input.GetMouseButtonUp(0)) {
-            movementLerpStep = 1 - (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) / playerSpeed);
-            startingVelocity = desiredVelocity;
-            desiredVelocity = Vector2.zero;
-        }
-
-        GetComponent<Rigidbody2D>().velocity = new Vector2(
-            Mathf.SmoothStep(startingVelocity.x, desiredVelocity.x, movementLerpStep),
-            GetComponent<Rigidbody2D>().velocity.y);
-
-        if (movementLerpStep < 1f) {
-            movementLerpStep += Time.deltaTime*acceleration;
-        } else {
-            movementLerpStep = 1f;
-        }
-
+        return ignore;
     }
 }
