a06f30353d78130f4a75bb5c1eb7996ee108fa0c
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2018-02-08 22:32:08 -0800

    Add Outline Rotation

---
+++ Abyss/Assets/Scripts/cameraRotationCopier.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraRotationCopier : MonoBehaviour {
+    public Camera cam;
+    private Quaternion offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = offset * cam.transform.rotation;
+	}
+}
